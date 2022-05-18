8a395794c85709679798b5dddffc5aed8a7c83ec
t4n-du4ng <dungnguyenq1w2@gmail.com>
2022-05-19 00:57:46 +0700

    add user info in header and handle dropdown menu for user

--- front-end/src/features/ManageExam/index.jsx
+++ front-end/src/features/ManageExam/index.jsx
@@ -1,18 +1,20 @@
-import React from "react";
-import { Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
-import CreateExam from "./pages/CreateExam";
-import EditExam from "./pages/EditExam";
+import React from 'react';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import CreateExam from './pages/CreateExam';
+import EditExam from './pages/EditExam';
+import ManageExam from './pages/ManageExam';
 
 function ManageExamFeature(props) {
-    return (
-        <div>
-            <Routes>
-                <Route path="/create" element={<CreateExam />} />
-                <Route path="/edit" element={<EditExam />} />
-            </Routes>
-        </div>
-    );
+	return (
+		<div>
+			<Routes>
+				<Route path="/" element={<ManageExam />} />
+				<Route path="/create" element={<CreateExam />} />
+				<Route path="/edit" element={<EditExam />} />
+			</Routes>
+		</div>
+	);
 }
 
 ManageExamFeature.propTypes = {};
---
+++ front-end/src/features/ManageExam/pages/ManageExam.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ManageExam() {
+    return <div>
+        
+    </div>;
+}
+
+export default ManageExam;
