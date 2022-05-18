982c78d7620f9b9030a08517c4cd262840f67254
TLCRATEAM <lengocdu123654tl@gmail.com>
2022-05-18 10:35:13 +0700

    add sign-in page

--- front-end/src/features/Exam/components/StateBox.jsx
+++ front-end/src/features/Exam/components/StateBox.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 
 function StateBox({ answers }) {
-	return (
-		<div>
-			{answers?.map((e, i) => (
-				<span className="box" key={i}>
-					{e == 0 ? "A" : e == 1 ? "B" : e == 2 ? "C" : e == 3 ? "D" : null}
-				</span>
-			))}
-		</div>
-	);
+    return (
+        <div>
+            {answers?.map((e, i) => (
+                <span className="box" key={i}>
+                    {e == 0
+                        ? "A"
+                        : e == 1
+                        ? "B"
+                        : e == 2
+                        ? "C"
+                        : e == 3
+                        ? "D"
+                        : null}
+                </span>
+            ))}
+        </div>
+    );
 }
 
 StateBox.propTypes = {};
--- front-end/src/features/Exam/pages/TakeExam.jsx
+++ front-end/src/features/Exam/pages/TakeExam.jsx
@@ -1,61 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import ExamForm from '../components/ExamForm';
-import StateBox from '../components/StateBox';
-import Timer from '../components/Timer';
+import React, { useEffect, useState } from "react";
+import ExamForm from "../components/ExamForm";
+import StateBox from "../components/StateBox";
+import Timer from "../components/Timer";
 
 function TakeExam() {
-	const [exam, setExam] = useState([
-		{
-			question: 'What is your name?',
-			chosenAnswer: 'B',
-			correctAnswer: 'A',
-			answers: ['Nguyễn Văn A', 'Phạm Thị B', 'Lê Văn C', 'Trần Thị D '],
-		},
-		{
-			question: 'How old are you?',
-			chosenAnswer: 'C',
-			correctAnswer: 'C',
-			answers: ['12', '18', '20', '22'],
-		},
-		{
-			question: 'Where are you live?',
-			chosenAnswer: 'C',
-			correctAnswer: 'B',
-			answers: ['TP. Hồ Chí Minh', 'Đồng Nai', 'Bình Dương', 'Khánh Hoà'],
-		},
-		{
-			question: 'Which major are you studying? ',
-			chosenAnswer: 'D',
-			correctAnswer: 'D',
-			answers: [
-				'Computer Sience',
-				'Information Systems',
-				'Information Technology',
-				'Software Engineering',
-			],
-		},
-	]);
-	const [answers, setAnswers] = useState([]);
-	// const [loading, setLoading] = useState(false);
+    const [exam, setExam] = useState([
+        {
+            question: "What is your name?",
+            chosenAnswer: "B",
+            correctAnswer: "A",
+            answers: ["Nguyễn Văn A", "Phạm Thị B", "Lê Văn C", "Trần Thị D "],
+        },
+        {
+            question: "How old are you?",
+            chosenAnswer: "C",
+            correctAnswer: "C",
+            answers: ["12", "18", "20", "22"],
+        },
+        {
+            question: "Where are you live?",
+            chosenAnswer: "C",
+            correctAnswer: "B",
+            answers: ["TP. Hồ Chí Minh", "Đồng Nai", "Bình Dương", "Khánh Hoà"],
+        },
+        {
+            question: "Which major are you studying? ",
+            chosenAnswer: "D",
+            correctAnswer: "D",
+            answers: [
+                "Computer Sience",
+                "Information Systems",
+                "Information Technology",
+                "Software Engineering",
+            ],
+        },
+    ]);
+    const [answers, setAnswers] = useState([]);
+    // const [loading, setLoading] = useState(false);
 
-	useEffect(() => {
-		setAnswers(new Array(exam.length).fill());
-	}, []);
-	useEffect(() => {}, [answers]);
-	const handleChooseAnswer = (index, answer) => {
-		const newAnswers = [...answers];
-		newAnswers[index] = answer;
-		setAnswers(newAnswers);
-	};
-	return (
-		<div className="exam">
-			<ExamForm exam={exam} handleChooseAnswer={handleChooseAnswer} />
-			<div>
-				<StateBox answers={answers} />
-				<Timer />
-			</div>
-		</div>
-	);
+    useEffect(() => {
+        setAnswers(new Array(exam.length).fill());
+    }, []);
+    useEffect(() => {}, [answers]);
+    const handleChooseAnswer = (index, answer) => {
+        const newAnswers = [...answers];
+        newAnswers[index] = answer;
+        setAnswers(newAnswers);
+    };
+    return (
+        <div className="exam">
+            <ExamForm exam={exam} handleChooseAnswer={handleChooseAnswer} />
+            <div>
+                <StateBox answers={answers} />
+                <Timer />
+            </div>
+        </div>
+    );
 }
 
 export default TakeExam;
