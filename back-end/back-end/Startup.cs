95b4054421f6e538509ee6325dc8e3ee6d055535
t4n-du4ng <dungnguyenq1w2@gmail.com>
2022-05-30 15:15:01 +0700

    test database connection

--- back-end/back-end/Startup.cs
+++ back-end/back-end/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,7 +27,7 @@ namespace back_end
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddTransient<MySqlConnection>(_ => new MySqlConnection(Configuration["ConnectionStrings:DbCon"]));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
