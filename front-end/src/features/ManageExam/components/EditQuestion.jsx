af19dc96096267d6915b06c13080980c8ecf2112
t4n-du4ng <dungnguyenq1w2@gmail.com>
2022-06-24 01:13:40 +0700

    Update EditExam API

--- front-end/src/features/ManageExam/components/EditQuestion.jsx
+++ front-end/src/features/ManageExam/components/EditQuestion.jsx
@@ -10,21 +10,22 @@ function EditQuestion({ register, errors, label, content, correctAnswerId, answe
 	// console.log(content, correctAnswerId, answerList);
 	const labelQuestion = label + 'content';
 	const labelCorrectAnswerId = label + 'correctAnswerId';
-	const labelAnswerList = [
-		label + 'answerA',
-		label + 'answerB',
-		label + 'answerC',
-		label + 'answerD',
-	];
+	// const labelAnswerList = [
+	// 	label + 'answerA',
+	// 	label + 'answerB',
+	// 	label + 'answerC',
+	// 	label + 'answerD',
+	// ];
+	const labelAnswerList = label + 'answerListChange';
 	// const [checkBox, setCheckBox] = useState(false);
 
 	const [questionChange, setQuestionChange] = useState(content);
 	const [correctAnswerChange, setCorrectAnswerChange] = useState(correctAnswerId);
 	const [answerListChange, setAnswerListChange] = useState([
-		answerList != undefined ? answerList[0]?.content : null,
-		answerList != undefined ? answerList[1]?.content : null,
-		answerList != undefined ? answerList[2]?.content : null,
-		answerList != undefined ? answerList[3]?.content : null,
+		answerList !== undefined ? answerList[0]?.content : null,
+		answerList !== undefined ? answerList[1]?.content : null,
+		answerList !== undefined ? answerList[2]?.content : null,
+		answerList !== undefined ? answerList[3]?.content : null,
 	]);
 
 	const updateAnswerListChanged = (index) => (e) => {
@@ -33,14 +34,13 @@ function EditQuestion({ register, errors, label, content, correctAnswerId, answe
 
 		setAnswerListChange(newArr);
 	};
-
 	return (
 		<div className="w-full">
 			<ErrorMessage
 				errors={errors}
 				name={labelQuestion}
 				render={() => (
-					<span className="bg-red-200 py-1 px-2 rounded text-red-900 font-semibold">
+					<span className="bg-red-200 py-1 px-2 rounded text-red-900 font-semibold text-base">
 						Hãy thêm nội dung cho câu hỏi.
 					</span>
 				)}
@@ -58,49 +58,105 @@ function EditQuestion({ register, errors, label, content, correctAnswerId, answe
 				errors={errors}
 				name={labelCorrectAnswerId}
 				render={() => (
-					<span className="bg-red-200 py-1 px-2 rounded text-red-900 font-semibold">
+					<span className="bg-red-200 py-1 px-2 rounded text-red-900 font-semibold text-base">
 						Hãy chọn đáp án đúng.
 					</span>
 				)}
 			/>
 			<div className="flex flex-col mt-4">
-				<div className="mb-4">
-					<label className="flex items-center relative mt-2">
-						<input
-							className="mr-3 h-6 w-6 "
-							{...register(labelCorrectAnswerId, {
-								required: true,
-							})}
-							// type={checkBox ? 'checkbox' : 'radio'}
-							type="radio"
-							value="0"
-							checked={correctAnswerChange == '0'}
-							onClick={(e) => {
-								setCorrectAnswerChange(e.target.value);
-							}}
-						/>
-						<span className="absolute -top-5 left-14 text-lg px-3 font-semibold">
-							Đáp án A
-						</span>
-						<textarea
-							className="mx-3 my-1 h-14 p-2 w-full rounded-xl border border-gray-500 bg-transparent text-lg focus:outline-none"
-							{...register(labelAnswerList[0], { required: true })}
-							type="text"
-							value={answerListChange[0]}
-							onChange={updateAnswerListChanged(0)}
-						/>
-					</label>
-					<ErrorMessage
-						errors={errors}
-						name={labelAnswerList[0]}
-						render={() => (
-							<span className="bg-red-200 py-1 px-2 rounded text-red-900 font-semibold">
-								Hãy thêm nội dung cho đáp án A.
-							</span>
-						)}
-					/>
-				</div>
-				<div className="mb-4">
+				{answerList
+					? answerList.map((e, i) => (
+							<div className="mb-4" key={i}>
+								<label className="flex items-center relative mt-2">
+									<input
+										className="mr-3 h-6 w-6 "
+										{...register(labelCorrectAnswerId, {
+											required: true,
+										})}
+										// type={checkBox ? 'checkbox' : 'radio'}
+										type="radio"
+										value={e.id}
+										checked={correctAnswerChange == e.id}
+										onClick={(e) => {
+											setCorrectAnswerChange(e.target.value);
+										}}
+									/>
+									<span className="absolute -top-5 left-14 text-lg px-3 font-semibold">
+										Đáp án {i === 0 ? 'A' : i === 1 ? 'B' : i === 2 ? 'C' : 'D'}
+									</span>
+									<textarea
+										className="mx-3 my-1 h-14 p-2 w-full rounded-xl border border-gray-500 bg-transparent text-lg focus:outline-none"
+										{...register(`${labelAnswerList}.${i}`, { required: true })}
+										type="text"
+										value={answerListChange[i]}
+										onChange={updateAnswerListChanged(i)}
+									/>
+								</label>
+								<ErrorMessage
+									errors={errors}
+									name={`${labelAnswerList}.${i}`}
+									render={() => (
+										<span className="bg-red-200 py-1 px-2 rounded text-red-900 font-semibold text-base">
+											Hãy thêm nội dung cho đáp án{' '}
+											{i === 0 ? 'A' : i === 1 ? 'B' : i === 2 ? 'C' : 'D'}.
+										</span>
+									)}
+								/>
+							</div>
+					  ))
+					: Array(4)
+							.fill()
+							.map((e, i) => (
+								<div className="mb-4" key={i}>
+									<label className="flex items-center relative mt-2">
+										<input
+											className="mr-3 h-6 w-6 "
+											{...register(labelCorrectAnswerId, {
+												required: true,
+											})}
+											// type={checkBox ? 'checkbox' : 'radio'}
+											type="radio"
+											value={i}
+											checked={correctAnswerChange == i}
+											onClick={(e) => {
+												setCorrectAnswerChange(e.target.value);
+											}}
+										/>
+										<span className="absolute -top-5 left-14 text-lg px-3 font-semibold">
+											Đáp án{' '}
+											{i === 0 ? 'A' : i === 1 ? 'B' : i === 2 ? 'C' : 'D'}
+										</span>
+										<textarea
+											className="mx-3 my-1 h-14 p-2 w-full rounded-xl border border-gray-500 bg-transparent text-lg focus:outline-none"
+											{...register(`${labelAnswerList}.${i}`, {
+												required: true,
+											})}
+											type="text"
+											value={answerListChange[i]}
+											onChange={updateAnswerListChanged(i)}
+										/>
+									</label>
+									<ErrorMessage
+										errors={errors}
+										name={`${labelAnswerList}.${i}`}
+										render={() => (
+											<span className="bg-red-200 py-1 px-2 rounded text-red-900 font-semibold text-base">
+												Hãy thêm nội dung cho đáp án{' '}
+												{i === 0
+													? 'A'
+													: i === 1
+													? 'B'
+													: i === 2
+													? 'C'
+													: 'D'}
+												.
+											</span>
+										)}
+									/>
+								</div>
+							))}
+
+				{/* <div className="mb-4">
 					<label className="flex items-center relative mt-2">
 						<input
 							className="mr-3 h-6 w-6 "
@@ -207,7 +263,7 @@ function EditQuestion({ register, errors, label, content, correctAnswerId, answe
 							</span>
 						)}
 					/>
-				</div>
+				</div> */}
 			</div>
 		</div>
 	);
--- front-end/src/features/ManageExam/pages/EditExam.jsx
+++ front-end/src/features/ManageExam/pages/EditExam.jsx
@@ -5,7 +5,87 @@ import Loading from '../../../components/Loading';
 import EditExamForm from '../components/EditExamForm';
 
 function EditExam() {
-	const [exam, setExam] = useState({});
+	const [exam, setExam] = useState({
+		id: 18,
+		name: 'Đề thi Toán 1',
+		maxDuration: 60,
+		createdTime: '2022-06-23T19:43:35',
+		teacherId: 10,
+		subjectId: 1,
+		numOfQuestions: 3,
+		questionList: [
+			{
+				id: 19,
+				content: '1 + 1 = ',
+				correctAnswerId: 58,
+				answerList: [
+					{
+						id: 57,
+						content: '3',
+					},
+					{
+						id: 58,
+						content: '2',
+					},
+					{
+						id: 59,
+						content: '3',
+					},
+					{
+						id: 60,
+						content: '4',
+					},
+				],
+			},
+			{
+				id: 20,
+				content: '1 + 2 = ',
+				correctAnswerId: 63,
+				answerList: [
+					{
+						id: 61,
+						content: '1',
+					},
+					{
+						id: 62,
+						content: '2',
+					},
+					{
+						id: 63,
+						content: '3',
+					},
+					{
+						id: 64,
+						content: '4',
+					},
+				],
+			},
+			{
+				id: 21,
+				content: '1 + 3 = ',
+				correctAnswerId: 68,
+				answerList: [
+					{
+						id: 65,
+						content: '1',
+					},
+					{
+						id: 66,
+						content: '2',
+					},
+					{
+						id: 67,
+						content: '3',
+					},
+					{
+						id: 68,
+						content: '4',
+					},
+				],
+			},
+			,
+		],
+	});
 	const { examId } = useParams();
 
 	const [loading, setLoading] = useState(false);
@@ -21,8 +101,9 @@ function EditExam() {
 						Authorization: `Bearer ${token}`,
 					},
 				});
-                if (res.data) {
-                    console.log(res.data)
+
+				if (res.data) {
+					console.log(res.data);
 					setExam(res.data);
 					setLoading(false);
 				}
@@ -34,7 +115,11 @@ function EditExam() {
 		fetchExam();
 	}, [examId]);
 
-	return <div className="p-5">{loading ? <Loading /> : <EditExamForm exam={exam} />}</div>;
+	return (
+		<div className="p-5">
+			{loading ? <Loading /> : <EditExamForm examId={examId} exam={exam} />}
+		</div>
+	);
 }
 
 export default EditExam;
